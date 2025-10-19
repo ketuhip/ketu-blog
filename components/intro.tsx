03f634dd542fd112cc17413bdb6278b3f6b88221
ketuhip <ketuhip@hotmail.com>
2025-10-19 17:08:13 +0900

    Hello-World

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,20 +2,13 @@ import { CMS_NAME } from "../lib/constants";
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        ketuのブログ
+    <section className="flex-col md:flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+        ketu Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4>
+      <h2 className="text-left md:text-left text-base mt-5 md:pl-8">
+        投稿者の日常ややったことを書くブログ
+      </h2>
     </section>
   );
 };
